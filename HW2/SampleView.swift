a6a7fec98363f19a81884a21897fc4ffb8a311b4
Onur Parlak <onur.parlak@OnurParlak.local>
2021-12-25 12:49:25 +0300

    initial commit

---
+++ HW2/SampleView.swift
@@ -0,0 +1,38 @@
+//
+//  SampleView.swift
+//  HW2
+//
+//  Created by Onur Parlak on 25.12.2021.
+//
+
+import Foundation
+
+import UIKit
+
+final class SampleView: UIView {
+
+    @IBOutlet private var contentView: UIView!
+    @IBOutlet private weak var textField: UITextField!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialize()
+    }
+
+    private func initialize() {
+        Bundle.main.loadNibNamed("SampleView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+
+    @IBAction func onSubmitButtonTapped(_ sender: UIButton) {
+        let text = textField.text
+    }
+
+}
---
+++ HW2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HW2
+//
+//  Created by Onur Parlak on 25.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sampleView: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
